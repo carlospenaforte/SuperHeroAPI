98a25f31a48fc46ba7d6777e78d5a2f9bf3ba7c7
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-20 17:01:42 -0300

    data, connect with server(json), entities, programcs, extensions and migrations.

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+using SuperHeroAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
